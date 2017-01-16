ceeaee48ad79bcd94c6e9afb378b12a1bfe5cd51
ldevl <rr30b@mail.ru>
2017-01-16 23:25:49 +0300

    Update banbot.lua

--- plugins/banbot.lua
+++ plugins/banbot.lua
@@ -1,18 +1,9 @@
---[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
---]]
-
 do
 
 local function run(msg, matches)
 local bot_id = our_id 
 local receiver = get_receiver(msg)
-    if matches[1] == 'زحلك'  and is_admin1(msg) then
+    if matches[1] == 'غادر'  and is_admin1(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
 	   leave_channel(receiver, ok_cb, false)
     end
@@ -20,18 +11,10 @@ end
  
 return {
   patterns = {
-    "^(زحلك)$",
-    "^[!#/](زحلك)$",
+    "^(غادر)$",
+    "^[!#/](غادر)$",
     "^!!tgservice (.+)$",
   },
   run = run
 }
 end
---[[ 
-    _____    _        _    _    _____    Dev @lIMyIl 
-   |_   _|__| |__    / \  | | _| ____|   Dev @li_XxX_il
-     | |/ __| '_ \  / _ \ | |/ /  _|     Dev @h_k_a
-     | |\__ \ | | |/ ___ \|   <| |___    Dev @Aram_omar22
-     |_||___/_| |_/_/   \_\_|\_\_____|   Dev @IXX_I_XXI
-              CH > @lTSHAKEl_CH
---]]
