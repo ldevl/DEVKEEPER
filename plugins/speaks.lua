5dfdd6846fc10dc9649a40c1f6b3cf64b44eb649
ldevl <ldevl@users.noreply.github.com>
2017-02-04 14:41:00 +0300

    Update speaks.lua

--- plugins/speaks.lua
+++ plugins/speaks.lua
@@ -9,7 +9,7 @@ if ( msg.text ) then
 
   if ( msg.to.type == "user" ) then
 
-    local text = "نورت/ي عزيزي♥️❗️ @"..msg.from.username..'\n\nانت/يْ بالفعــــل تتحدٌث 🗣 مہعہ البــــوت ✔️♥️❗️ \n\n للتحدث مہٰع المطور 🕵 \n\n @llX8Xll 💔\n\nآذآ كنــتٰ محظور 🗣 تعـٰال هنــاا حبيــبي💋 @llX8Xllbot\n\n♥️❗️قہنہآه👨🏿‍💻 آلسہؤرسہ ςђ @DEV_KEEPER_NEW\n\nقہنہآه 🗣  تابعـــةة للمطور🕵🏾 يہرجہآ الاشتراك @xx_KR_HM_xx  🍾🍷\n'     reply_msg(reply_id, text, ok_cb, false)
+    local text = "نورت/ي عزيزي♥️❗️ @"..msg.from.username..'\n\nانت/يْ بالفعــــل تتحدٌث 🗣 مہعہ البــــوت ✔️♥️❗️ \n\n للتحدث مہٰع المطور 🕵 \n\n @llX8Xll 💔\n\nآذآ كنــتٰ محظور 🗣 تعـٰال هنــاا حبيــبي💋 @lZlxlZlBOT\n\n♥️❗️قہنہآه👨🏿‍💻 آلسہؤرسہ ςђ @keeper_ch\n\nقہنہآه 🗣  تابعـــةة للمطور🕵🏾 يہرجہآ الاشتراك @xx_KR_HM_xx  🍾🍷\n'     reply_msg(reply_id, text, ok_cb, false)
   end 
    
 end 
