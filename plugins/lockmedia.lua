c49b9cd62f93ff9cb67c2375e37141b84b55b0bf
ldevl <ldevl@users.noreply.github.com>
2017-02-21 01:28:43 +0300

    Update lockmedia.lua

--- plugins/lockmedia.lua
+++ plugins/lockmedia.lua
@@ -12,7 +12,7 @@ local chat = msg.to.id
     if redis:get(moody) and msg.media and not is_momod(msg) then 
 
             delete_msg(msg.id, ok_cb, false) 
-local test = " عزيزي😼 ["..msg.from.first_name.."]".."\n".."لا تمســـ🗣ـــلت (الصــوٌر_والفيــديـُوهات_والصـٰٰـٰٰٓوتياتْ) تہمہ منــعها ✖️ مسبقــٰاً  فــٰي المجموعـــــٰةة 🌐 لآ تكُــرر الامر كہيہ 😽 لآ تجبرنــــيْ على طردك 🕵🏻 اتبعٌْ القوانييين♥️❗️".."\n".." 🔰 Us : @"..(msg.from.username or " ") 
+local test = " عزيزي😼 ["..msg.from.first_name.."]".."\n".."لا تمســـ🗣ـــلت (الصــوٌر_والفيــديـُوهات_والصـٰٰـٰٰٓوتياتْ) تہمہ منــعها ✖️ مسبقــٰاً  فــٰي المجموعـــــٰةة 🌐 لآ تكُــرر الامر كہيہ 😽 لآ تجبرنــــيْ على طردك 🕵🏻 اتبعٌْ القوانييين♥️❗️".."\n".." ♦️ Us : @"..(msg.from.username or " ") 
 reply_msg(mohammed, test, ok_cb, true) 
 
 end 
@@ -26,7 +26,7 @@ local mohammed = msg['id']
     if matches[1] == 'قفل الوسائط'  and is_momod(msg) then 
                     local th3boss= 'mate:'..msg.to.id 
                     redis:set(th3boss, true) 
-local boss = '|تـــم بالفعـــل #قفــل |🔐 الوسائـــــط فــي 🕵🏻 هذه المجموعـةة❤️❗️ \n🔲♦️Us : @'..(msg.from.username or " لآ يہؤجہد ")..'\n🔲♦️ID : '.. msg.from.id..'\n'
+local boss = 'تـــم بالفعـــل #قفــل |🔐| الوسائـــــط فــي 🕵🏻 هذه المجموعـةة❤️❗️ \n🔲♦️Us : @'..(msg.from.username or " لآ يہؤجہد ")..'\n🔲♦️ID : '.. msg.from.id..'\n'
 reply_msg(mohammed, boss, ok_cb, true) 
 elseif matches[1] == 'قفل الوسائط' and not is_momod(msg) then 
 local moody = 'للمشرفيين  🕵🏻 فقــط عزيـــزي♥️❗️' 
@@ -34,7 +34,7 @@ reply_msg(mohammed, moody, ok_cb, true)
   elseif is_momod(msg) and matches[1] == 'فتح الوسائط' then 
       local th3boss= 'mate:'..msg.to.id 
       redis:del(th3boss) 
-local boss = '|تـــم بالفعــل #فتـــح |🔓 الوسائــــط فـي 🕵🏻 المجموعـــةة♥️❗️\n🔲♦️Us : @'..(msg.from.username or " لآ يہؤجہد ")..'\n🔲♦️ID : '.. msg.from.id..'\n'
+local boss = 'تـــم بالفعــل #فتـــح |🔓| الوسائــــط فـي 🕵🏻 المجموعـــةة♥️❗️\n🔲♦️Us : @'..(msg.from.username or " لآ يہؤجہد ")..'\n🔲♦️ID : '.. msg.from.id..'\n'
 reply_msg(mohammed, boss, ok_cb, true) 
 elseif matches[1] == 'فتح الوسائط' and not is_momod(msg) then 
 local moody= ' للمشرفيــن 🕵🏻 فقــط عزيزي♥️❗️ ' 
